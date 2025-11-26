a2b40642773fa5b3755766a25310d05b97a02008
Bill Garrett <garrett@astralfrontier.org>
2025-11-25 20:34:53 -0800

    Add MDX support

---
+++ src/Navbar.tsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <>
+      <header className="container">
+        <nav>
+          <ul>
+            <li>
+              <strong>Breathtaking Comics</strong>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
