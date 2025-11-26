82691010e8712c961a8f7e2525e3d79410d11e22
Bill Garrett <garrett@astralfrontier.org>
2025-11-26 01:32:02 -0800

    Add previous rolls to random roll tool

--- src/rules/RandomRollTool.tsx
+++ src/rules/RandomRollTool.tsx
@@ -2,12 +2,22 @@ import { useCallback, useState } from "react";
 import { randomValue } from "../traits";
 
 function RandomRollTool() {
-  const [roll, setRoll] = useState<number>(0);
+  const [rolls, setRolls] = useState<number[]>([randomValue(6) + 1]);
 
-  const rolld4 = useCallback(() => setRoll(randomValue(4) + 1), []);
-  const rolld6 = useCallback(() => setRoll(randomValue(6) + 1), []);
-  const rolld8 = useCallback(() => setRoll(randomValue(8) + 1), []);
-  const rolld10 = useCallback(() => setRoll(randomValue(10) + 1), []);
+  function addRoll(newRoll: number) {
+    setRolls((rolls) => {
+      const newRolls = [newRoll, ...rolls];
+      while (newRolls.length > 5) {
+        newRolls.pop();
+      }
+      return newRolls;
+    });
+  }
+
+  const rolld4 = useCallback(() => addRoll(randomValue(4) + 1), []);
+  const rolld6 = useCallback(() => addRoll(randomValue(6) + 1), []);
+  const rolld8 = useCallback(() => addRoll(randomValue(8) + 1), []);
+  const rolld10 = useCallback(() => addRoll(randomValue(10) + 1), []);
 
   return (
     <article>
@@ -18,26 +28,30 @@ function RandomRollTool() {
         <button onClick={rolld10}>d10</button>
       </p>
       <p>
-        {roll == 0 && <p>Click on a die to roll it</p>}
-        {roll > 0 && <strong>Result: {roll}</strong>}
-        {roll >= 1 && roll <= 2 && (
+        {rolls[0] == 0 && <p>Click on a die to roll it</p>}
+        {rolls[0] > 0 && (
+          <>
+            <strong>Result:</strong> {rolls.join(", ")}
+          </>
+        )}
+        {rolls[0] >= 1 && rolls[0] <= 2 && (
           <p>
             <strong>1-2</strong>: one Drive flips to Red - explain what went
             wrong!
           </p>
         )}
-        {roll >= 3 && roll <= 4 && (
+        {rolls[0] >= 3 && rolls[0] <= 4 && (
           <p>
             <strong>3-4</strong>: one Drive flips to Red, another Drive flips to
             Green - mixed success
           </p>
         )}
-        {roll >= 5 && roll <= 9 && (
+        {rolls[0] >= 5 && rolls[0] <= 9 && (
           <p>
             <strong>5-9</strong>: one Drive flips to Green - success!
           </p>
         )}
-        {roll >= 10 && (
+        {rolls[0] >= 10 && (
           <p>
             <strong>10+</strong>: two Drives flip to Green - great success!
           </p>
