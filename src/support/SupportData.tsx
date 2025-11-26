b3e463f4e870a09f5a98afec14489d512328ccc6
Bill Garrett <garrett@astralfrontier.org>
2025-11-25 22:28:10 -0800

    Add setting entries

--- src/support/SupportData.tsx
+++ src/support/SupportData.tsx
@@ -1,11 +1,24 @@
 import type { BreathtakingAsset } from "../AssetTypes";
+
 import Action from "./action.mdx";
+import ClarionHeights from "./clarion-heights.mdx";
+import ZetaCity from "./zeta-city.mdx";
 
 const supportData: BreathtakingAsset[] = [
   {
-    name: "ACTION",
+    name: "A.C.T.I.O.N.",
     slug: "action",
     element: <Action />,
+  },
+  {
+    name: "Clarion Heights Academy and Miller Mountain Corrections",
+    slug: "clarion-heights",
+    element: <ClarionHeights />,
+  },
+  {
+    name: "Zeta City",
+    slug: "zeta-city",
+    element: <ZetaCity />,
   },
 ];
 
