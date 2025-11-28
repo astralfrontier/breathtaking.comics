0f5dfda75d1fb9ffe6e831abf9a2fcb9d641b1a6
Bill Garrett <garrett@astralfrontier.org>
2025-11-28 02:21:07 -0800

    Add several locations

--- src/AssetList.tsx
+++ src/AssetList.tsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router";
-import type { BreathtakingAssetPackage } from "./AssetTypes";
+import type {
+  BreathtakingAsset,
+  BreathtakingAssetGroup,
+  BreathtakingAssetPackage,
+} from "./AssetTypes";
 import { groupBy } from "ramda";
 import { useMemo } from "react";
 
@@ -9,34 +13,52 @@ interface AssetListProps {
   children?: React.ReactNode;
 }
 
+// Only return the asset's group if it exists and also exists in the list of groups
+const assetIsInGroups = (groups: BreathtakingAssetGroup[]) => {
+  const groupSlugs = groups.map((group) => group.slug);
+  return (asset: BreathtakingAsset): string => {
+    if (!asset.group) {
+      return "";
+    } else if (!groupSlugs.includes(asset.group)) {
+      return "";
+    } else {
+      return asset.group;
+    }
+  };
+};
+
+const NULL_GROUP: BreathtakingAssetGroup = {
+  slug: "",
+  name: "",
+  description: "",
+};
+
 function AssetList(props: AssetListProps) {
   const { prefix, assetPackage, children } = props;
 
   const groupedAssets = useMemo(
-    () => groupBy((asset) => asset.group ?? "", assetPackage.assets),
-    [assetPackage.assets]
+    () => groupBy(assetIsInGroups(assetPackage.groups), assetPackage.assets),
+    [assetPackage.assets, assetPackage.groups]
   );
 
   return (
     <>
       {children}
-      {[{ slug: "", name: "", description: "" }, ...assetPackage.groups].map(
-        (group) => (
-          <>
-            {group.name && <h1>{group.name}</h1>}
-            {group.description && <p>{group.description}</p>}
-            {groupedAssets[group.slug] && (
-              <ul>
-                {groupedAssets[group.slug]?.map((asset) => (
-                  <li>
-                    <Link to={`/${prefix}/${asset.slug}`}>{asset.name}</Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </>
-        )
-      )}
+      {[NULL_GROUP, ...assetPackage.groups].map((group) => (
+        <>
+          {group.name && <h1>{group.name}</h1>}
+          {group.description && <p>{group.description}</p>}
+          {groupedAssets[group.slug] && (
+            <ul>
+              {groupedAssets[group.slug]?.map((asset) => (
+                <li>
+                  <Link to={`/${prefix}/${asset.slug}`}>{asset.name}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      ))}
     </>
   );
 }
--- src/support/SupportData.tsx
+++ src/support/SupportData.tsx
@@ -5,28 +5,67 @@ import type {
 } from "../AssetTypes";
 
 import Action from "./action.mdx";
+import CaptainBurger from "./captain-burger.mdx";
 import ClarionHeights from "./clarion-heights.mdx";
+import UniversalConsulting from "./universal-consulting.mdx";
+import Wavelength from "./wavelength.mdx";
 import ZetaCity from "./zeta-city.mdx";
+
+const SETTINGS_GROUP = "settings";
+const ORGANIZATIONS_GROUP = "organizations";
 
 const supportAssets: BreathtakingAsset[] = [
   {
     name: "A.C.T.I.O.N.",
     slug: "action",
+    group: ORGANIZATIONS_GROUP,
     element: <Action />,
+  },
+  {
+    name: "Captain Burger",
+    slug: "captain-burger",
+    element: <CaptainBurger />,
   },
   {
     name: "Clarion Heights Academy and Miller Mountain Corrections",
     slug: "clarion-heights",
+    group: SETTINGS_GROUP,
     element: <ClarionHeights />,
+  },
+  {
+    name: "Universal Consulting",
+    slug: "universal-consulting",
+    group: ORGANIZATIONS_GROUP,
+    element: <UniversalConsulting />,
+  },
+  {
+    name: "Wavelength",
+    slug: "wavelength",
+    group: ORGANIZATIONS_GROUP,
+    element: <Wavelength />,
   },
   {
     name: "Zeta City",
     slug: "zeta-city",
+    group: SETTINGS_GROUP,
     element: <ZetaCity />,
   },
 ];
 
-const supportGroups: BreathtakingAssetGroup[] = [];
+const supportGroups: BreathtakingAssetGroup[] = [
+  {
+    name: "Organizations",
+    slug: ORGANIZATIONS_GROUP,
+    description:
+      "Teams, groups, or factions that can make appearances, independent of the overall setting.",
+  },
+  {
+    name: "Settings",
+    slug: SETTINGS_GROUP,
+    description:
+      "Places, times, or situations where you can set a story arc, or an entire game.",
+  },
+];
 
 const supportPackage = {
   assets: supportAssets,
